002ebf6f6ac4b3bfbd522d3b5f87cb5927fc8b9e
joao chavez <joao.chavez@pucp.pe>
2017-03-24 19:54:15 -0400

    Added animation for one button

--- RCApp/TipDetailsViewController.swift
+++ RCApp/TipDetailsViewController.swift
@@ -13,7 +13,14 @@ class TipDetailsViewController: UIViewController {
     @IBOutlet weak var tipImage: UIImageView!
     @IBOutlet weak var descriptionLabel: UILabel!
     
+    @IBOutlet weak var moreInfoButton: UIButton!
+    
+    
+    
+    @IBOutlet weak var menuHeightConstraint: NSLayoutConstraint!
+    
     var tip: TipModel?
+    var isMenuOpen = false
     
     @IBAction func OpenInBrowser(sender: AnyObject) {
         if let url = NSURL(string: (tip?.sourceUrl)!){
@@ -26,8 +33,43 @@ class TipDetailsViewController: UIViewController {
         UIPasteboard.general.string = tip?.sourceUrl
     }
     
+    @IBAction func openMenu(_ sender: UIButton) {
+        isMenuOpen = !isMenuOpen
+        
+        menuHeightConstraint.constant = isMenuOpen ? 75.0 : 30.0
+        
+        moreInfoButton.isHidden = !isMenuOpen
+    
+        UIView.animate(withDuration: 1.0, delay: 0,
+                       
+                       usingSpringWithDamping: 0.4, initialSpringVelocity: 10.0,
+                       
+                       options: [.curveEaseOut], animations: {
+                        
+                        self.view.layoutIfNeeded()
+                        let angle = self.isMenuOpen ? CGFloat(0.2): 0
+                        self.moreInfoButton.transform = CGAffineTransform(rotationAngle: angle)
+                        
+        }, completion: nil)
+        
+        
+        
+//        UIView.animate(withDuration: 0.33, delay: 0,
+//                       options: .curveEaseOut,
+//                       animations: {
+//                            self.view.layoutIfNeeded()
+//                        //let angle = self.isMenuOpen ? CGFloat(M_PI_4) : 0
+//                        let angle = self.isMenuOpen ? CGFloat(0.5): 0
+//                        self.moreInfoButton.transform = CGAffineTransform(rotationAngle: angle)
+//                        },
+//                       completion: nil)
+        
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        moreInfoButton.isHidden = !isMenuOpen
         
         if let selectedTip = tip {
             navigationItem.title = selectedTip.name
@@ -37,7 +79,9 @@ class TipDetailsViewController: UIViewController {
     }
     
     override func viewDidLoad() {
-        super.viewDidLoad()        
+        super.viewDidLoad()
+        
+       // moreInfoButton.isHidden = false
     }
 
     override func didReceiveMemoryWarning() {
