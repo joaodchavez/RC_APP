2ed9034a8ae759350b9ea3a3da69ae87f72b2793
joao chavez <joao.chavez@pucp.pe>
2017-06-05 01:27:47 -0400

    Created enums file and added useful enums

---
+++ RCApp/Enums.swift
@@ -0,0 +1,16 @@
+//
+//  Enums.swift
+//  RCApp
+//
+//  Created by Joao Chavez on 6/5/17.
+//  Copyright © 2017 Fuzz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum WeatherIcon: String{
+    case Cloudy = "ic_wb_cloudy"
+    case Clear = "ic_wb_sunny"
+    case Rainy = "ic_grain"
+}
